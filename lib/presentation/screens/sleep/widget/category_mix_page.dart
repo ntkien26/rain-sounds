32c0c7950c14efb842068c2747a21f88355424cc
j3nn13 <Admin@TrangTheCat-2304 MINGW64 ~>
2023-09-14 18:54:01 +0700

    fix dialog unlock for free

--- lib/presentation/screens/sleep/widget/category_mix_page.dart
+++ lib/presentation/screens/sleep/widget/category_mix_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rain_sounds/common/configs/app_cache.dart';
 import 'package:rain_sounds/common/injector/app_injector.dart';
 import 'package:rain_sounds/common/utils/ad_helper.dart';
@@ -132,136 +133,199 @@ class _MixItemState extends State<MixItem> {
               context: context,
               builder: (context) {
                 final Size size = MediaQuery.of(context).size;
-                return AlertDialog(
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12.0))),
-                  backgroundColor: bgColor,
-                  content: SizedBox(
-                    width: size.width * 0.9,
-                    height: size.height * 0.6,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
-                          child: Align(
-                              alignment: Alignment.topRight,
-                              child: SvgPicture.asset(IconPaths.icClose)),
-                        ),
-                        const Text(
-                          'Unlock for free',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        Container(
-                          height: size.height * 0.2,
-                          alignment: Alignment.bottomCenter,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage(
-                                  '${Assets.baseImagesPath}/${widget.mix.cover?.thumbnail}.webp'),
-                              fit: BoxFit.cover,
-                            ),
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(6)),
+                return Dialog(
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                    elevation: 16,
+                    child: SingleChildScrollView(
+                      child: Container(
+                        height: size.height/1.35,
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                          border: Border.all(
+                            color: k202968,
+                            width: 2,
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Text(
-                              widget.mix.name ?? '',
-                              style: const TextStyle(color: Colors.white),
-                            ),
+                          gradient: const LinearGradient(
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight,
+                            transform: GradientRotation(5.50),
+                            colors: [
+                              k181E4A,
+                              k202968,
+                            ],
                           ),
                         ),
-                        const SizedBox(
-                          height: 32,
-                        ),
-                        const Text(
-                          'Watch a short video to get this premium mix util close app',
-                          style: TextStyle(color: Colors.white),
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            unlockMix(adHelper, soundService, context);
-                          },
-                          child: Container(
-                            height: size.height * 0.175,
-                            decoration: const BoxDecoration(
-                              color: Colors.brown,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(12)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            // InkWell(
+                            //   onTap: () {
+                            //     Navigator.pop(context);
+                            //   },
+                            //   child: Align(
+                            //       alignment: Alignment.topRight,
+                            //       child: SvgPicture.asset(IconPaths.icClose)),
+                            // ),
+                            const SizedBox(
+                              height: 24,
                             ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SvgPicture.asset(IconPaths.icPlay),
-                                const SizedBox(
-                                  width: 12,
+                            const Text(
+                              'Unlock for free',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(32),
+                              height: size.width * 0.65,
+                              width: size.width * 0.65,
+                              alignment: Alignment.bottomCenter,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                      '${Assets.baseImagesPath}/${widget.mix.cover?.thumbnail}.webp'),
+                                  fit: BoxFit.cover,
                                 ),
-                                const Text(
-                                  'WATCH',
-                                  style: TextStyle(color: Colors.white),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        Container(
-                            height: size.height * 0.175,
-                            decoration: const BoxDecoration(
-                              gradient: LinearGradient(
-                                begin: Alignment.bottomLeft,
-                                end: Alignment.topRight,
-                                colors: [
-                                  kGradientOrangeBtColor,
-                                  kGradientPurpleBtColor,
-                                ],
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(16)),
                               ),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(12)),
                             ),
-                            child: InkWell(
-                              onTap: () {
-                                getIt<NavigationService>().navigateToScreen(
-                                    screen: const InAppPurchaseScreen());
-                              },
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    height: 32,
-                                    width: 32,
-                                    child: SvgPicture.asset(
-                                        IconPaths.icPremiumNoColor),
-                                  ),
-                                  const SizedBox(
-                                    width: 12,
-                                  ),
-                                  const Text(
-                                    'GO PREMIUM',
-                                    style: TextStyle(color: Colors.white),
-                                  )
-                                ],
+                            const SizedBox(
+                              height: 24,
+                            ),
+                            SizedBox(
+                              width: size.width * 0.65,
+                              child: const Text(
+                                'Watch a short video to get this premium mix util close app',
+                                style: TextStyle(color: Colors.white),
+                                textAlign: TextAlign.center,
                               ),
-                            )),
-                      ],
-                    ),
-                  ),
-                );
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.all(12).copyWith(bottom: 0),
+                              child: TextButton(
+                                onPressed: () {
+                                  unlockMix(adHelper, soundService, context);
+                                },
+                                style: TextButton.styleFrom(
+                                  padding: EdgeInsets.zero,
+                                  tapTargetSize:
+                                      MaterialTapTargetSize.shrinkWrap,
+                                  side: const BorderSide(
+                                    color: k7F65F0,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                ),
+                                child: Ink(
+                                  padding: EdgeInsets.zero,
+                                  decoration:  const BoxDecoration(
+                                    gradient: LinearGradient(
+                                      begin: Alignment.bottomLeft,
+                                      end: Alignment.topRight,
+                                      colors: <Color>[
+                                        k5C40DF,
+                                        k7F65F0,
+                                      ],
+                                    ),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(100)),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 16, horizontal: 16),
+                                    child: SizedBox(
+                                      width: size.width - 32,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          SvgPicture.asset(IconPaths.icPlay),
+                                          const SizedBox(
+                                            width: 12,
+                                          ),
+                                          const Text(
+                                            'Watch',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.all(12).copyWith(bottom: 0),
+                              child: TextButton(
+                                onPressed: () {
+                                  // widget.onSaveCustomClick(sounds);
+                                  getIt<NavigationService>().navigateToScreen(
+                                      screen: const InAppPurchaseScreen());
+                                },
+                                style: TextButton.styleFrom(
+                                  padding: EdgeInsets.zero,
+                                  tapTargetSize:
+                                      MaterialTapTargetSize.shrinkWrap,
+                                  side: const BorderSide(
+                                    color: kDEAA21,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                ),
+                                child: Ink(
+                                  padding: EdgeInsets.zero,
+                                  decoration: const BoxDecoration(
+                                    gradient: LinearGradient(
+                                      begin: Alignment.bottomLeft,
+                                      end: Alignment.topRight,
+                                      transform: GradientRotation(5.50),
+                                      colors: [
+                                        kDEAA21,
+                                        kFFE144,
+                                      ],
+                                    ),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(100)),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 16, horizontal: 16),
+                                    child: SizedBox(
+                                      width: size.width - 32,
+                                      child: Text(
+                                        'Go Premium',
+                                        textAlign: TextAlign.center,
+                                        style: GoogleFonts.nunito(
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ));
               });
         } else {
           getIt<NavigationService>()
